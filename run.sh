ff824c9aa3c5965bbb54eed53eeeef6cb2f3b16c
timetraveller_san <anonymous.yolo.wtf@gmail.com>
2020-01-15 20:07:00 +0000

    run.sh update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python train.py --save_dir $1 --epochs 20 --run_name seresnext50 --model_name se_resnext50_32x4d --rgb True --activation mish --mixup True --cutmix True --continue_train True 
+python train.py --save_dir $1 --epochs $2 --run_name seresnext50 --model_name se_resnext50_32x4d --rgb True --activation mish --mixup True --cutmix True --continue_train True
 # python train.py --epochs 20 --run_name seresnext50 --model_name efficientnet-b0 --rgb True --activation mish --debug True
